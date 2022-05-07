a9066a0b909f87e40987b1cb3c581fd89f7c580d
Mathieu Bergeron <mathieu.bergeron@cmontmorency.qc.ca>
2022-05-07 10:30:44 -0400

    Exploration Godot

---
+++ exploration_godot01/CSGBox.gd
@@ -0,0 +1,30 @@
+extends CSGBox
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+func _input(event):
+	if event is InputEventKey:
+		if event.scancode == KEY_LEFT:
+			self.translate(Vector3(-4,0,0))
+		elif event.scancode == KEY_RIGHT:
+			self.translate(Vector3(4,0,0))
+		elif event.scancode == KEY_UP:
+			self.translate(Vector3(0,4,0))
+		elif event.scancode == KEY_DOWN:
+			self.translate(Vector3(0,-4,0))
+		elif event.scancode == KEY_A:
+			self.translate(Vector3(0,0,-4))
+		elif event.scancode == KEY_Z:
+			self.translate(Vector3(0,0,4))
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
